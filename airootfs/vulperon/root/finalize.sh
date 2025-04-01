d9b3a59bb343bf2e1f796af4f8a6d3d2bf08c08c
Tokard <58886109+TokardDev@users.noreply.github.com>
2025-04-01 13:56:47 +0200

    added font

--- airootfs/vulperon/root/finalize.sh
+++ airootfs/vulperon/root/finalize.sh
@@ -5,6 +5,17 @@ systemctl enable bluetooth
 systemctl enable cronie
 systemctl enable timeshift-firstboot.service
 
+sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/"$/ splash"/' /etc/default/grub && grub-mkconfig -o /boot/grub/grub.cfg
+sed -i '/^HOOKS=/ s/\b\(encrypt\|sd-encrypt\)\b/plymouth &/' /etc/mkinitcpio.conf
+
+if ! grep -q 'plymouth' /etc/mkinitcpio.conf; then
+    sed -i '/^HOOKS=/ s/)/ plymouth)/' /etc/mkinitcpio.conf
+fi
+
+plymouth-set-default-theme spinner
+
+mkinitcpio -P
+
 dconf update
 echo "Post-install configuration done !"
 
